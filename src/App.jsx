ddcf34639de3d646730711b323c511496f920f3c
svixco <andrei.montaniel.cics@ust.edu.ph>
2025-05-31 19:42:51 +0800

    gh page test

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import MonthlyCalendar from './Components/MonthlyCalendar';
 import Welcomepage from './Components/Welcomepage';
 import AuthRedirect from './Components/AuthRedirect'; // ✅ Don't forget this!
+import { HashRouter as Router } from 'react-router-dom';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
